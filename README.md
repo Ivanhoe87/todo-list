f44e2667e3b0f4501f5a2644d5bb9bfb9e6e910f
Ivanhoe <gamaivanhoe@gmail.com>
2019-11-28 19:16:58 -0400

    primera version

---
+++ README.md
@@ -0,0 +1,1 @@
+# To do list class exercise.
---
+++ src/js/component/home.js
@@ -0,0 +1,98 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			tasks: [
+				"Hacer café",
+				"Hacer la cama",
+				"Tomar agua",
+				"Regar las plantas"
+			],
+			newTask: ""
+		};
+		this.handleAddTask = this.handleAddTask.bind(this);
+		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleDeleteTask = this.handleDeleteTask.bind(this);
+	}
+	handleAddTask(e) {
+		e.preventDefault();
+		let task = e.target.value;
+		this.setState({
+			tasks: [...this.state.tasks, this.state.newTask],
+			newTask: ""
+		});
+	}
+	handleInputChange(e) {
+		this.setState({
+			...this.state,
+			newTask: e.target.value
+		});
+	}
+	handleDeleteTask(e, indexToDelete) {
+		let tasksLeft = this.state.tasks.filter(
+			(value, index) => index != indexToDelete
+		);
+		this.setState({
+			tasks: tasksLeft,
+			newTask: this.state.newTask
+		});
+	}
+	render() {
+		let tasks = this.state.tasks;
+		return (
+			<div className="container d-flex flex-column">
+				<header className="todo-header text-center mb-3">
+					<h1 className="display-3">Mi lista de tareas</h1>
+				</header>
+				<section className="todo-body">
+					<form onSubmit={this.handleAddTask} className="text-center">
+						<input
+							className="my-5 mx-auto display-4"
+							placeholder="Agrega más tareas!"
+							onChange={this.handleInputChange}
+							value={this.state.newTask}
+						/>
+					</form>
+					<ul className="main-list mx-auto">
+						{tasks.map((value, index) => {
+							return (
+								<li
+									key={index}
+									className="list-item display-4 my-2 mx-0">
+									{value}
+									<span
+										onClick={e =>
+											this.handleDeleteTask(e, index)
+										}
+										className="delete-button"
+									/>
+								</li>
+							);
+						})}
+					</ul>
+					<footer className="list-footer mx-auto mt-5">
+						<p>
+							{this.state.tasks.length > 1
+								? `Faltan ${
+										this.state.tasks.length
+								  } tareas por hacer...`
+								: this.state.tasks.length == 1
+									? `Falta ${
+											this.state.tasks.length
+									  } sola tarea por hacer!`
+									: "Felicitaciones, ya hiciste todo!!! Viva el ocio!!!"}
+						</p>
+					</footer>
+				</section>
+			</div>
+		);
+	}
+}
+
+//create your first component
+export default Home;
